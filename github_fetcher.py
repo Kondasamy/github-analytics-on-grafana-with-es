8a8918c5d9229c8e63b93bc89ba7ad34ca90db6e
kondasamy@outlook.com <kondsit008!>
2018-09-20 18:10:08 +0530

    Adding elasticsearch push and punch-card data processing unit

--- github_fetcher.py
+++ github_fetcher.py
@@ -1,8 +1,9 @@
 import configparser
 import requests
 import logging
-from datetime import datetime
-from elasticsearch import Elasticsearch
+import uuid
+from datetime import datetime, date, time
+from elasticsearch import Elasticsearch, helpers, RequestError
 
 
 # Logger config
@@ -34,25 +35,70 @@ def get_github_punch_card_data():
         print(f'Firing the GET call - {url}')
         response = requests.get(url, auth_headers)
         if response.status_code == 200:
-            process_punch_data_for_es(response.json())
+            process_punch_data_for_es(key, response.json())
         else:
             logger.warn(f"Failed attempt with status code : {response.status_code}")
 
 
-def process_punch_data_for_es(response):
+def process_punch_data_for_es(key, response):
     """Structurize punch card data 
     """
-    pass
+    # Elastic search connection
+    es_config = get_config('ELASTICSEARCH')
+    _es = Elasticsearch([{'host': es_config['host'], 'port': int(es_config['port'])}])
 
+    today = int(datetime.combine(date.today(), time(0, 0, 0)).timestamp()) # unix timestamp
+    past_eigth_day = today - (86400*8) # 8 days * 86400 seconds
+        
+    if _es.ping():
+        # Setting the mapping of elasticsearch index
+        if not _es.indices.exists(es_config['index']):
+            mapping = '''
+            {
+                "mappings": {
+                    "punch_card": {
+                        "properties": {
+                            "commits": {
+                                "type": "long"
+                            },
+                            "repo": {
+                                "type": "text",
+                                "fields": {
+                                    "keyword": {
+                                        "type": "keyword",
+                                        "ignore_above": 256
+                                    }
+                                }
+                            },
+                            "timestamp": {
+                                "type": "date",
+                                "format": "epoch_second"
+                            }
+                        }
+                    }
+                }
+            }
+            '''
+            _es.indices.create(index=es_config['index'], body= mapping)
 
-def push_data_to_es():
-    pass
+        # Process and populate data to Elasticsearch
+        for hour in response:
+            timestamp = past_eigth_day+3600
+            past_eigth_day = timestamp
+            data = {
+                    'repo': key,
+                    'commits': hour[2],
+                    'timestamp': timestamp
+                }
+            try:
+                _es.index(index=es_config['index'], doc_type='punch_card', id=f"{key}_{timestamp}", body=data)
+            except RequestError as re:
+                print(re.info)
+                   
+    else:
+        logger.warn("Failed attempt connecting elasticsearch instance")
 
 
 if __name__ == '__main__':
-    print(get_config('REPOS'))
-    # get_github_punch_card_data()
-    # if response.status_code == 200:
-    #     print(response.json())
-    # else:
-    #     logger.warn("Failed fetching punch card details!")
+    print(get_config('REPOS')) # 1537284600
+    get_github_punch_card_data()
